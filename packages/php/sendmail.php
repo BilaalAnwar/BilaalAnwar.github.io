26a2b07e108fe46b694c2f09b423c26ac8d2c3e8
BilaalAnwar <32581768+BilaalAnwar@users.noreply.github.com>
2024-09-10 15:25:02 +0500

    website

---
+++ packages/php/sendmail.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $description = trim($_POST["description"]);
+
+    $recipient = "bilaalanwar0@gmail.com";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$description\n";
+
+    $email_headers = "From: $name <$email>\r\n";
+    $email_headers .= "Reply-To: $email\r\n";
+    $email_headers .= "MIME-Version: 1.0\r\n";
+    $email_headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
